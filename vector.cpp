9abf4ad7edcadbb654143ba2a3274013ae633139
Lew <59511836+reseeded@users.noreply.github.com>
2020-11-04 13:31:30 -0500

    Update vector.cpp
    Second edit

--- vector.cpp
+++ vector.cpp
@@ -1,8 +1,26 @@
 #pragma once
 
 #include <cmath>
+#include <Windows.h>
+#include <cstdint>
 
-struct vector
+
+struct vector3d
 {
-
+  //initialize coordinates
+uintptr_t x, y, z;
+  
+//overloads for formula here:
+float x2, y2, z2; //for mathematical purposes
+  float x = (x - x2, y - y2, z -z2);
+  float y = (x + x2, y + y2, z + z2);
+  float z = (x * x2, y * y2, z * z2);
+  
+  //end overloads
+  
+float vector3d, magnitude;
+//re-initialize vector3d and initialize magnitude
+  
+magnitude = sqrt(x^2 + y^2 + z^2);
+//calculate magnitude 
 };
