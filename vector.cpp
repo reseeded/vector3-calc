11f5e388905f4a6574f0c39959cb737d808ba437
Lew <59511836+reseeded@users.noreply.github.com>
2020-11-04 19:37:49 -0500

    Update vector.cpp
    fixing errors

--- vector.cpp
+++ vector.cpp
@@ -12,23 +12,33 @@ float maxAngleV = 89;
 
 struct vector3
 {
-  //initialize coordinates
-uintptr_t x, y, z;
-  
-//overloads for formula here:
-float x2, y2, z2; //for mathematical purposes
-  uintptr_t alt(x2, y2, z2) = 0;
-  
-  float x = (x - x2, y - y2, z -z2);
-  float y = (x + x2, y + y2, z + z2);
-  float z = (x * x2, y * y2, z * z2);
-  
-  //end overloads
-  
-float vector3d, magnitude;
-//re-initialize vector3d and initialize magnitude
-  
-magnitude = sqrt(x^2 + y^2 + z^2);
-//calculate magnitude 
-return 0;
+    //initialize coordinates
+    uintptr_t x, y, z;
+
+    //overloads for formula here:
+
+    float x2, y2, z2 = 0.0f; //alternative variables for vector calculation
+
+
+    float x = (x - x2, y - y2, z - z2);
+    float y = (x + x2, y + y2, z + z2);
+    float z = (x * x2, y * y2, z * z2);
+
+    //end overloads
+ 
+    float vector3d, magnitude;
+    //re-initialize vector3d and initialize magnitude
+    
+    float magnitude = sqrt(x ^ 2 + y ^ 2 + z ^ 2);
+    //calculate magnitude 
+    
 };
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Initialized\n";
+    
+    return 0;
+}
