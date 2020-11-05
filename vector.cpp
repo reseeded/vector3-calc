bc08c29b4fb887421058e01b186e6a4a68f3c2c3
Lew <59511836+reseeded@users.noreply.github.com>
2020-11-04 19:54:39 -0500

    Update vector.cpp
    Compiles now

--- vector.cpp
+++ vector.cpp
@@ -14,19 +14,18 @@ struct vector3
 {
     //initialize coordinates
     uintptr_t x, y, z;
-
     //overloads for formula here:
 
     float x2, y2, z2 = 0.0f; //alternative variables for vector calculation
 
 
-    float x = (x - x2, y - y2, z - z2);
-    float y = (x + x2, y + y2, z + z2);
-    float z = (x * x2, y * y2, z * z2);
+    uintptr_t ix = (x - x2, y - y2, z - z2);
+    uintptr_t iy = (x + x2, y + y2, z + z2);
+    uintptr_t iz = (x * x2, y * y2, z * z2);
 
     //end overloads
  
-    float vector3d, magnitude;
+    float vector3d;
     //re-initialize vector3d and initialize magnitude
     
     float magnitude = sqrt(x ^ 2 + y ^ 2 + z ^ 2);
